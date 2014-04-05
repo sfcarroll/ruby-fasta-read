eef77b2fc9481ea27a44553751d2941b4021988d
Andrea D'Amico <andrea.damico@gmail.com>
2014-04-05 14:15:08 +0200

    passing features

--- lib/fasta_read/sequence.rb
+++ lib/fasta_read/sequence.rb
@@ -31,31 +31,5 @@ module FastaRead
       number / 5
     end
 
-    #def foo
-      #snp = masked_unmasked(@options)[0]
-      #subst = masked_unmasked(@options)[1]
-      #path = "fasta/#{@assembly}/#{snp}/chr#{@chromosome + subst}.fa"
-      #debug("About to read #{@assembly} #{snp} chr#{@chromosome}:#{@cstart}-#{@cend}")
-      #exit_now_with!(assembly_or_chromosome(@assembly, @chromosome)) unless File.exist?(path)
-      #if @options[:output]
-        #File.open(@options[:output], "w") do |file|
-          #file.puts IO.read(path, @cend.to_i - @cstart.to_i, @cstart.to_i)
-        #end
-      #else
-        #puts IO.read(path, @cend.to_i - @cstart.to_i, @cstart.to_i)
-      #end
-    #end
-
-    def masked_unmasked(options)
-      options[:snp] ? ["snp", ".subst"] : ["unmasked", ""]
-    end
-
-    def assembly_or_chromosome(assembly, chromosome)
-      Dir.exist?("fasta/#{assembly}") ? [chromosome, "chromosome"] : [assembly, "assembly"]
-    end
-
-    def exit_now_with!(value_and_argument)
-      exit_now!(1, "the '#{value_and_argument[0]}' #{value_and_argument[1]} doesn't exist in directory structure")
-    end
   end
 end
